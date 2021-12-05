78d363dc3708257b3002ab7fff36fb5e764f7a9b
Enrique Moreno <jemc300x@gmail.com>
2021-12-04 21:04:20 -0500

    add api

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Usuario } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private apiUrl = 'https://reqres.in/api';
+
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<Usuario[]> {
+    return this.http.get(`${this.apiUrl}/users?per_page=6`).pipe(map((resp: any) => resp.data));
+  }
+}
